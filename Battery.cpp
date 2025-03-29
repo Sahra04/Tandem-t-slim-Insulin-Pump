f41f61997df8d135b2bf70650dd52b62fff2ac15
Navitha Kokkat <navitha.tkokkat@gmail.com>
2025-03-29 16:41:05 -0400

    testing battery print

---
+++ Battery.cpp
@@ -0,0 +1,8 @@
+#include "Battery.h"
+#include <string>
+#include <iostream>
+
+void Battery::navithaprint(){
+    std::cout << "This is the navithaprint function." << std::endl;
+
+}
--- Battery.h
+++ Battery.h
@@ -1,4 +1,9 @@
 #ifndef BATTERY_H
 #define BATTERY_H
 
+class Battery {
+public:
+    void navithaprint();
+};
+
 #endif // BATTERY_H
