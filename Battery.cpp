44445b6ae639b0bdcd2f5cd6bc3e9995f814456f
Stefany Gao <stefany.gao1@gmail.com>
2025-03-29 16:57:13 -0400

    Stefany's test

---
+++ Battery.cpp
@@ -0,0 +1,5 @@
+#include "Battery.h"
+
+void Battery::stefanyPrint() {
+    std::cout << "Hello";
+}
--- Battery.h
+++ Battery.h
@@ -1,4 +1,10 @@
 #ifndef BATTERY_H
 #define BATTERY_H
+#include <iostream>
+class Battery {
+private:
 
+public:
+    void stefanyPrint();
+};
 #endif // BATTERY_H
